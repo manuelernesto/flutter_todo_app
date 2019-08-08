873d8a43e0345774e887a0438609408425170d05
manuelernesto <manuelernesto@outlook.com>
2019-08-08 16:16:27 +0100

    setup to-do project

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _toDoList = [];
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To-Do"),
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "New To-do",
+                        labelStyle: TextStyle(color: Colors.purple)),
+                  ),
+                ),
+                RaisedButton(
+                    color: Colors.purple,
+                    child: Text("Add"),
+                    textColor: Colors.white,
+                    onPressed: () {})
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
