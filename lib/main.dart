23f4c267d0128fd7f42c641debb017a3dfef300f
manuelernesto <manuelernesto@outlook.com>
2019-08-11 18:38:29 +0100

    finishing my to-do flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _controller = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved = Map();
+  int _lastRemovedPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _toDoList = jsonDecode(data);
+      });
+    });
+  }
+
+  void _addTodo() {
+    setState(() {
+      Map<String, dynamic> newTodo = Map();
+      newTodo["title"] = _controller.text;
+      newTodo["ok"] = false;
+      _controller.text = "";
+      _toDoList.add(newTodo);
+      _saveData();
+    });
+  }
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
@@ -38,6 +62,78 @@ class _HomeState extends State<Home> {
     }
   }
 
+  Widget _buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.info),
+        ),
+        onChanged: (isChecked) {
+          setState(() {
+            _toDoList[index]["ok"] = isChecked;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPosition = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snackbar = SnackBar(
+            content: Text("\"${_lastRemoved['title']}\" removed"),
+            action: SnackBarAction(
+                label: "undo",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPosition, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snackbar);
+        });
+      },
+    );
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,6 +150,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                    controller: _controller,
                     decoration: InputDecoration(
                         labelText: "New To-do",
                         labelStyle: TextStyle(color: Colors.purple)),
@@ -63,10 +160,17 @@ class _HomeState extends State<Home> {
                     color: Colors.purple,
                     child: Text("Add"),
                     textColor: Colors.white,
-                    onPressed: () {})
+                    onPressed: _addTodo)
               ],
             ),
-          )
+          ),
+          Expanded(
+              child: RefreshIndicator(
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(top: 10),
+                      itemCount: _toDoList.length,
+                      itemBuilder: _buildItem),
+                  onRefresh: _refresh))
         ],
       ),
     );
